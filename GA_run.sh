6ebed5283323e0380834ce16b034ff9694b6c96b
Christo Sad <74500978+CriZ-sd@users.noreply.github.com>
2022-03-08 14:56:08 +0200

    Update GA_run.sh

--- GA_run.sh
+++ GA_run.sh
@@ -8,85 +8,120 @@ echo 'fifth argument : nodes file name (current value :'$5')';
 echo 'sixth argument : edges file name (current value :'$6')';
 
 echo 'Starting' ;
-lifetime = 0;
-gbetter = 0;
-mincutper = 0;
+
+lifetime=0;
+gbetter=0;
+mincutper=0;
 
 if [ $1 -lt  500 ] ; then 
  if [ $2 -lt 3 ] ; then 
-  lifetime= 75;
-  gbetter= 0.00065;
-  mincutper= 0.4;
+  lifetime=75;
+  gbetter=0.00065;
+  mincutper=0.4;
  elif [ $2 -lt 4] ; then
   lifetime= 75;
-  gbetter= 0.00065;
-  mincutper= 0.6;
+  gbetter=0.00065;
+  mincutper=0.6;
  elif  [ $2 -lt 6 ] ; then
-  lifetime= 75;
-  gbetter= 0.00065;
-  mincutper= 0.7;
+  lifetime=75;
+  gbetter=0.00065;
+  mincutper=0.7;
  elif [ $2 -lt 8 ] ; then 
-  lifetime= 70;
-  gbetter= 0.00065;
-  mincutper= 0.8;
+  lifetime=70;
+  gbetter=0.00065;
+  mincutper=0.8;
  elif [ $2 -lt 11] ; then 
-  lifetime= 65;
-  gbetter= 0.00065;
-  mincutper= 1.6;
+  lifetime=65;
+  gbetter=0.00065;
+  mincutper=1.6;
  else
-  echo 'Untested partitioning please try a custom selection of parameters values using g++ and ./';
+  echo '====> Untested partitioning!! For better results  please try a custom selection of parameters values using g++ and ./';
+  lifetime=65;
+  gbetter=0.00065;
+  mincutper=1.6;
  fi
 elif [ $1 -lt 800 ] ; then 
  if [ $2 -lt 3 ] ; then 
-  lifetime= 65;
-  gbetter= 0.0065;
-  mincutper= 0.4;
+  lifetime=65;
+  gbetter=0.0065;
+  mincutper=0.4;
  elif [ $2 -lt 4] ; then
-  lifetime= 65;
-  gbetter= 0.0065;
-  mincutper= 0.6;
+  lifetime=65;
+  gbetter=0.0065;
+  mincutper=0.6;
  elif  [ $2 -lt 6 ] ; then
-  lifetime= 65;
-  gbetter= 0.0065;
-  mincutper= 0.7;
+  lifetime=65;
+  gbetter=0.0065;
+  mincutper=0.7;
  elif [ $2 -lt 8 ] ; then 
-  lifetime= 55;
-  gbetter= 0.0065;
-  mincutper= 0.8;
+  lifetime=55;
+  gbetter=0.0065;
+  mincutper=0.8;
  elif [ $2 -lt 11] ; then 
-  lifetime= 50;
-  gbetter= 0.0065;
-  mincutper= 1.6;
+  lifetime=50;
+  gbetter=0.0065;
+  mincutper=1.6;
  else
-  echo 'Untested partitioning please try a custom selection of parameters values using g++ and ./';
+   echo '====> Untested partitioning!! For better results  please try a custom selection of parameters values using g++ and ./';
+  lifetime=50;
+  gbetter=0.0065;
+  mincutper=1.6;
  fi
-if [ $1 -lt 1600 ] ; then 
+elif [ $1 -lt 1600 ] ; then 
  if [ $2 -lt 3 ] ; then 
-  lifetime= 55;
-  gbetter= 0.0065;
-  mincutper= 0.4;
- elif [ $2 -lt 4] ; then
-  lifetime= 55;
-  gbetter= 0.0065;
-  mincutper= 0.6;
+  lifetime=55;
+  gbetter=0.0065;
+  mincutper=0.4;
+ elif [ $2 -lt 4 ] ; then
+  lifetime=55;
+  gbetter=0.0065;
+  mincutper=0.6;
  elif  [ $2 -lt 6 ] ; then
-  lifetime= 55;
-  gbetter= 0.0065;
-  mincutper= 0.7;
+  lifetime=55;
+  gbetter=0.0065;
+  mincutper=0.7;
  elif [ $2 -lt 8 ] ; then 
-  lifetime= 45;
-  gbetter= 0.0065;
-  mincutper= 0.8;
- elif [ $2 -lt 11] ; then 
-  lifetime= 40;
-  gbetter= 0.0065;
-  mincutper= 1.6;
+  lifetime=45;
+  gbetter=0.0065;
+  mincutper=0.8;
+ elif [ $2 -lt 11 ] ; then 
+  lifetime=40;
+  gbetter=0.0065;
+  mincutper=1.6;
  else
-  echo 'Untested partitioning please try a custom selection of parameters values using g++ and ./';
+  echo '====> Untested partitioning!! For better results  please try a custom selection of parameters values using g++ and ./';
+  lifetime=40;
+  gbetter=0.0065;
+  mincutper=1.6;
  fi
 else 
-echo 'Untested number of nodes please try custom selection of parameters values using g++ and ./'
-
+echo '===> Untested number of nodes!! For better results please try custom selection of parameters values using g++ and ./'
+if [ $2 -lt 3 ] ; then 
+  lifetime=55;
+  gbetter=0.0065;
+  mincutper=0.4;
+ elif [ $2 -lt 4 ] ; then
+  lifetime=55;
+  gbetter=0.0065;
+  mincutper=0.6;
+ elif  [ $2 -lt 6 ] ; then
+  lifetime=55;
+  gbetter=0.0065;
+  mincutper=0.7;
+ elif [ $2 -lt 8 ] ; then 
+  lifetime=45;
+  gbetter=0.0065;
+  mincutper=0.8;
+ elif [ $2 -lt 11 ] ; then 
+  lifetime=40;
+  gbetter=0.0065;
+  mincutper=1.6;
+ else
+   echo '====> Untested partitioning!! For better results  please try a custom selection of parameters values using g++ and ./';
+  lifetime=40;
+  gbetter=0.0065;
+  mincutper=1.6;
+ fi
 fi
 
 
